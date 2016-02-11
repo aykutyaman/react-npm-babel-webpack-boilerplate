2ebe13841efdaf41b50fa67128f8db477446412e
Aykut Yaman <yaman.ayk@gmail.com>
2016-02-11 15:57:36 +0200

    initial commit

---
+++ examples/example.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import Hello from '../lib/index.jsx';
+
+render(<Hello/>, document.getElementById('hello'));
---
+++ lib/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Hello = React.createClass({
+  render: function() {
+    return (<div>world</div>)
+  }
+});
+
+export default Hello;
